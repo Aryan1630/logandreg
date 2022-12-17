d0a45a2a20ffeb69b7d6573ef51552493726b469
unknown <aryanav30@gmail.com>
2022-12-17 20:06:11 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+// any new lib import goes above this app initialization
+const app = express()
+const port = process.env.PORT || 3000
+
+const publicpath = path.join(__dirname, './public');
+app.use(express.static(publicpath));
+
+const templatePath = path.join(__dirname, '/templates/views')
+const partialsPath = path.join(__dirname, '/templates/partials')
+app.set('view engine', 'hbs')
+app.set('views', templatePath)
+hbs.registerPartials(partialsPath)
+
+app.get('/login', (req,res)=>{
+    res.render('login')
+})
+
+app.get('/register', (req,res)=>{
+    res.render('registration')
+})
+
+app.listen(port)
